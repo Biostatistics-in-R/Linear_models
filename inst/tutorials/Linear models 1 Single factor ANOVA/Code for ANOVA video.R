0bde9f4bd08c97eb1d0ace0c41060fe0409459a5
rjknell <r.knell@qmul.ac.uk>
2020-09-19 14:49:52 +0100

    Added tutorial on diagnostics, renumbered everything

--- inst/tutorials/Linear models 1 Single factor ANOVA/Code for ANOVA video.R
+++ inst/tutorials/Linear models 1 Single factor ANOVA/Code for ANOVA video.R
@@ -58,3 +58,63 @@ SStreatment1 <- SStotal1 - SSerror1
 
 A1 <- lm(var1 ~ F1)
 A2 <- lm(var2 ~ F1)
+
+set.seed(2116)
+F2 <- as.factor(rep(c("A", "B", "C"), each = 8))
+
+var3 <- c(rnorm(8, mean = 20, sd = 2),
+          rnorm(8, mean = 16, sd = 2),
+          rnorm(8, mean = 22, sd = 2))
+
+palette1 <- c("chocolate4", "aquamarine4", "firebrick3")
+
+plot(var3, col = palette1[F2],
+     pch= 16,
+     xaxt = "n",
+     xlab = "",
+     ylab = "value")
+
+legend(
+  "bottomleft",
+  legend = c("A", "B", "C"),
+  pch = 16,
+  col = palette1
+)
+
+
+abline(h = mean(var3), lwd = 2)
+
+arrows(x0 = 1:24, 
+       y0 = var3, 
+       x1 = 1:24,
+       y1 = rep(mean(var3),24),
+       length = 0,
+       col = palette1[F2])
+
+plot(var3, col = palette1[F2],
+     pch= 16,
+     xaxt = "n",
+     xlab = "",
+     ylab = "Value")
+
+legend(
+  "bottomleft",
+  legend = c("A", "B", "C"),
+  pch = 16,
+  col = palette1
+)
+
+mA <- mean(var3[F2 == "A"])
+mB <- mean(var3[F2 == "B"])
+mC <- mean(var3[F2 == "C"])
+
+lines(x = c(1,8), y = c(mA, mA), lwd = 2, col = palette1[1])
+lines(x = c(9,16), y = c(mB, mB), lwd = 2, col = palette1[2])
+lines(x = c(17,24), y = c(mC, mC), lwd = 2, col = palette1[3])
+
+arrows(x0 = 1:24, 
+       y0 = var3, 
+       x1 = 1:24,
+       y1 = rep(c(mA, mB, mC), each = 8),
+       length = 0,
+       col = palette1[F2])     
